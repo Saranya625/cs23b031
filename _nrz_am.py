21cd5585f6d68a143823cbc372266862be809127
Saranya <cs23b031@iittp.ac.in>
2026-01-06 15:06:37 +0530

    Implemented 20 Hz carrier signal
    Added NRZ coding and sinusoidal wave generation.

--- _nrz_am.py
+++ _nrz_am.py
@@ -1,3 +1,4 @@
+#NRZ-coding
 import numpy as np  
 import matplotlib.pyplot as plt                                                        
 data_sequence = [1, 0, 1, 0, 1, 1, 0]
@@ -13,3 +14,13 @@ plt.plot(nrz_signal)
 plt.xlabel('Sample')
 plt.ylabel('Amplitude')
 plt.show()
+#Sinusoidal wave - 20 Hz
+fc = 20 
+fs = bits_per_sample 
+
+T = len(nrz_signal) / fs
+t = np.linspace(0,T, len(nrz_signal), endpoint=False)
+
+carrier_signal = np.sin(2 * np.pi * fc * t)
+plt.plot(carrier_signal)
+ 
