ed595b05217fe8ce296e79a40b7ce7aa1c2fef38
Saranya <cs23b031@iittp.ac.in>
2026-01-06 14:35:38 +0530

    Implemented NRZ signal and plotting

--- _nrz_am.py
+++ _nrz_am.py
@@ -1,1 +1,15 @@
-
+import numpy as np  
+import matplotlib.pyplot as plt                                                        
+data_sequence = [1, 0, 1, 0, 1, 1, 0]
+bits_per_sample = 200
+nrz_signal = []
+for bit in data_sequence:
+  if bit == 1:
+    nrz_signal.extend([1] * bits_per_sample)
+  else:
+    nrz_signal.extend([-1] * bits_per_sample)
+nrz_signal = np.array(nrz_signal)
+plt.plot(nrz_signal)
+plt.xlabel('Sample')
+plt.ylabel('Amplitude')
+plt.show()
