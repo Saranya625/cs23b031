b23ab3cb4d53a46ad474abc488e2eb0fbde1120d
Saranya <cs23b031@iittp.ac.in>
2026-01-13 16:01:48 +0530

    implemented emperical function

---
+++ l2_cs23b031.py
@@ -0,0 +1,17 @@
+import random
+import plotext as plt
+n = 10
+trials = 100000
+p_values = [i / 10 for i in range(1, 11)]   
+
+empirical_probs = []
+analytical_probs = []
+
+for p in p_values:
+    success = 0
+    for _ in range(trials):
+        nodes = [random.random() < p for _ in range(n)]
+        if any(nodes):
+            success += 1
+
+    empirical = success / trials
